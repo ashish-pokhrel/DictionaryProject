590be82b1e27731d6ba0ea139829f5c01b5ea85f
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-05-12 23:49:23 -0500

    Dictionary Project Final

--- dict.js
+++ dict.js
@@ -1,1 +1,54 @@
-// styling for dict.html
+$(document).ready(function () {
+    const lookupButton = $("#lookup");
+    const searchBox = $("#searchBox");
+    const listBox = $("#search-list");
+    listBox.append(`<p> No Data Available. Please type the term and click lookup button.</p>`);
+
+    lookupButton.click(function () {
+        const searchedValue = searchBox.val();
+        $.ajax({
+            url: `/search-word?searchWord=${searchedValue}`,
+            type: "GET",
+            dataType: 'json',
+            success: function (result) {
+                const list = result.data;
+                listBox.empty();
+                if (list.length > 0) {
+                    for (let i = 0; i < list.length; i++) {
+                        const listVal = list[i];
+                        listBox.append(`<p>
+                    ${i + 1}(${listVal.wordtype})
+                    :: 
+                    ${listVal.definition}</p> <br>`);
+                    }
+                }
+                else{
+                    listBox.append(`<p> No Data Found ! </p>`);
+                }
+            },
+        })
+
+
+        // fetch(`/search-word?searchWord=${searchedValue}`).then((data) => {
+        //     console.log(data);
+        // })
+
+        // const xhr = new XMLHttpRequest();
+        // //open the object
+        // xhr.open('get', `/search-word?searchWord=${searchedValue}`, true)
+
+        // //on progress
+        // xhr.onprogress = function () {
+        //     console.log("on progress.........")
+        // }
+
+        // //when  response is ready
+        // xhr.getResponseHeader("Content-type", "application/json");
+        // xhr.onload = function() {
+        //     //console.log(this.responseText)
+        //     console.log("on progress..asdasda.......")
+        // }
+        // xhr.send();
+
+    })
+})
---
+++ sqlConfig.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",    // ip address of server running mysql
+    user: "root",    // user name to your mysql database
+    password: "12345678", // corresponding password
+    database: "entries"
+}
+
+
+module.exports = config;
